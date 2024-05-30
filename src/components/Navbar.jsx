5b097ac1583e8d0cddcaf0bcfc4434d3c4394ee7
Sumit Sahu <sssahu1009@gmail.com>
2024-05-30 23:03:45 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='logo'>
+            <img src="/images/logo2.jpg" alt="Logo" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
